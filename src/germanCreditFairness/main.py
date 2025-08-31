1358f4574e2d6380bab87953d200412f5097cedf
mms1410 <svenmauricemorlock@yahoo.com>
2025-09-01 00:00:43 +0200

    :sparkles: added training

--- src/germanCreditFairness/main.py
+++ src/germanCreditFairness/main.py
@@ -1,21 +1,58 @@
-import omegaconf
 from pathlib import Path
-from matplotlib import pyplot as plt
 from germanCreditFairness.data import get_german_credit
-from germanCreditFairness.plot_functions import plot_categories, plot_numerical
+from germanCreditFairness.utils import yaml_to_dictionary, decision_dict_to_frame, get_decision_combos, handle_decision_selection
+from germanCreditFairness.utils import PATH_CONF, PATH_ASSETS
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import csv
+import xgboost as xgb
 
-path_assets = Path(__file__).resolve().parent.parent.parent / Path("assets")
+SEED = 123
 
 credit = get_german_credit()
-# preprocessing_strategies = omegaconf.OmegaConf.load("preprocessing_strategies.yaml")
-# print(omegaconf.OmegaConf.to_container(preprocessing_strategies, resolve = True))
+print(f"total number of variables: {len(credit.columns)}")
+#------------------------------------ multiverse strategy setup -------------------------------------------------
+decisions = yaml_to_dictionary(PATH_CONF / Path("decisions.yaml"))
+
+decisions = decision_dict_to_frame(decisions)
+
+decisions_combo = get_decision_combos(decisions)
+
+print(f"possible decision combinations: {decisions_combo.shape[0]}, for {decisions_combo.shape[1]} varivables to change")
+#------------------------------------ multiverse analysis ------------------------------------------------------
+params = yaml_to_dictionary(PATH_CONF / Path("train.yaml"))
+params_xgb = params.get("params_xgb")
+model = xgb.XGBClassifier(params_xgb)
+
+rowcounter = 0
+str_log = PATH_ASSETS / Path("log.csv")
+all_columns = credit.columns.tolist()
+with open(str_log, "w", newline = "") as f:
+    writer = csv.writer(f)
+    writer.writerow(all_columns + ["accuracy"])
+
+for rownumber in range(len(decisions_combo)):
+    rowcounter += 1
+    new_decision = decisions_combo.iloc[[rownumber]]
+    credit_preprocessed = credit.copy()
+    credit_preprocessed = handle_decision_selection(new_decision, credit_preprocessed)
+
+    for col in credit_preprocessed.select_dtypes(include="category"):
+        credit_preprocessed[col] = credit_preprocessed[col].cat.codes
+    
+    X = credit_preprocessed.drop(columns = ["class"])
+    y = credit_preprocessed["class"]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state = SEED)
+
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    acc = accuracy_score(y_test, y_pred)
+    
+    # logging
+    active_covariates =[col if col in all_columns else "None" for col in credit_preprocessed.columns]
+    with open(str_log, "a", newline = "") as f:
+        writer = csv.writer(f)
+        writer.writerow(active_covariates + acc)
 
 
-# plot_categories(credit)
-# plt.savefig(path_assets / Path("credit_categories.png"), dpi = 300, bbox_inches = "tight")
-# plt.close()
-
-
-plot_numerical(credit)
-plt.savefig(path_assets / Path("credit_numericals.png"), dpi = 300, bbox_inches = "tight")
-plt.close()
+print(f" Number of different preprocessing alternatives: {rowcounter}")
